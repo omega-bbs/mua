0f3f0f5651d28b2da4c701c9ce85189d37771473
IntPtr <lxy.lixiaoyan@gmail.com>
2017-08-10 20:34:12 +0800

    add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure } from '@storybook/react'
+
+const req = require.context('../src', true, /\.stories\.js$/)
+
+configure(() => {
+  req.keys().forEach(filename => {
+    req(filename)
+  })
+}, module)
