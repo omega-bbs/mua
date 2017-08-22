607f3b5a3e44c11c09796e668a93fd604b9827bc
IntPtr <lxy.lixiaoyan@gmail.com>
2017-08-22 20:08:42 +0800

    fix Portal

--- src/ui/components/Portal.js
+++ src/ui/components/Portal.js
@@ -4,6 +4,7 @@ import { unstable_createPortal as createPortal } from 'react-dom'
 
 class Portal extends React.Component {
   static propTypes = {
+    target: PropTypes.object,
     children: PropTypes.node,
   }
 
@@ -15,7 +16,16 @@ class Portal extends React.Component {
     this.setState({ mounted: true })
   }
 
+  componentWillUnmount() {
+    if (this.target) {
+      this.target.remove()
+    }
+  }
+
   getTarget() {
+    if (this.props.target) {
+      return this.props.target
+    }
     if (!this.target) {
       this.target = document.createElement('div')
       document.body.appendChild(this.target)
