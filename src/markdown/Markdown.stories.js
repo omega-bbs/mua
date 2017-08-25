a3b64a925b9b34cdc0999041ea5df44e3b703116
IntPtr <lxy.lixiaoyan@gmail.com>
2017-08-26 00:53:07 +0800

    remove storybook

--- src/markdown/Markdown.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import Markdown from "./Markdown";
-
-storiesOf("Markdown", module).add("simple", () =>
-  <Markdown markdown="*italic* **bold** `code`" />,
-);
