10b1451689e9096ddf6a5ad2c338be7a14a61300
IntPtr <lxy.lixiaoyan@gmail.com>
2017-08-31 23:02:42 +0800

    Revert "remove spec: true"
    This reverts commit e0e3c2d03611dfb8c2b51c2ebd2f14f4bc6e66aa.

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 const production = process.env.NODE_ENV === "production";
 
 module.exports = {
-  presets: [["env", { modules: false }], "stage-0", "react"],
+  presets: [["env", { spec: true, modules: false }], "stage-0", "react"],
   plugins: [
     ["transform-runtime", { polyfill: false, useBuiltIns: true }],
     ["styled-components", { ssr: true, displayName: !production }],
