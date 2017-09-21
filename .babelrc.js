e079eda8604c35a12451ef628bda60a009a71876
IntPtr <lxy.lixiaoyan@gmail.com>
2017-09-21 18:08:02 +0800

    update .babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,12 @@
 const production = process.env.NODE_ENV === "production";
+const test = process.env.NODE_ENV === "test";
 
 module.exports = {
   presets: [["env", { modules: false }], "stage-0", "react"],
   plugins: [
+    test && "transform-es2015-modules-commonjs",
     ["transform-runtime", { polyfill: false, useBuiltIns: true }],
     ["styled-components", { ssr: true, displayName: !production }],
     "react-hot-loader/babel",
-  ],
-  env: {
-    test: {
-      plugins: ["transform-es2015-modules-commonjs"],
-    },
-  },
+  ].filter(Boolean),
 };
