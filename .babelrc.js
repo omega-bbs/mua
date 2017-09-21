8a73a705a366e2e0bc316a354067fcb03a5b2604
IntPtr <lxy.lixiaoyan@gmail.com>
2017-09-21 18:26:28 +0800

    update .babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -4,15 +4,15 @@ const test = process.env.NODE_ENV === "test";
 module.exports = {
   presets: [["env", { modules: false }], "stage-0", "react"],
   plugins: [
-    test && "transform-es2015-modules-commonjs",
+    ["transform-runtime", { polyfill: false, useBuiltIns: true }],
+    ["styled-components", { ssr: true, displayName: !production }],
+    production && "graphql-tag",
+    "react-hot-loader/babel",
     !production && "transform-react-jsx-source",
     production && [
       "transform-react-remove-prop-types",
       { ignoreFilenames: ["node_modules"] },
     ],
-    ["transform-runtime", { polyfill: false, useBuiltIns: true }],
-    ["styled-components", { ssr: true, displayName: !production }],
-    production && "graphql-tag",
-    "react-hot-loader/babel",
+    test && "transform-es2015-modules-commonjs",
   ].filter(Boolean),
 };
