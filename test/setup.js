3cc7987e45c0bfcde75aead65357c8e8bfd35894
IntPtr <lxy.lixiaoyan@gmail.com>
2017-10-25 03:19:04 +0800

    use test instead of jest as npm script name

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,7 @@
+global.requestAnimationFrame = callback => setTimeout(callback, 0);
+
 import { useStrict } from "mobx";
 import { useStaticRendering } from "mobx-react";
-
-global.requestAnimationFrame = callback => setTimeout(callback, 0);
 
 useStrict(true);
 useStaticRendering(true);
